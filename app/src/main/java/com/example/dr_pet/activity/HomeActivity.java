454a09ba1e669e9ad39502579a09d7f89c20adba
MaccaRinn <nghoangvietanh382004@gmail.com>
2025-07-21 16:14:58 +0700

    fix bug in home activity with admin

--- app/src/main/java/com/example/dr_pet/activity/HomeActivity.java
+++ app/src/main/java/com/example/dr_pet/activity/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.exifinterface.media.ExifInterface;
 import androidx.fragment.app.Fragment;
 
 import com.example.dr_pet.R;
@@ -12,6 +13,14 @@ import com.example.dr_pet.fragment.HomeFragment;
 import com.example.dr_pet.fragment.PetFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.dr_pet.model.AuthManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,9 +35,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        checkIfUserIsAdmin();
 
         bottomNavigationView = findViewById(R.id.bottom_menu);
-
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
 
@@ -36,9 +45,6 @@ public class HomeActivity extends AppCompatActivity {
         fragmentMap.put(R.id.nav_home, new HomeFragment());
         fragmentMap.put(R.id.nav_profile, new AccountFragment());
         fragmentMap.put(R.id.nav_petprofile, new PetFragment());
-
-
-
         loadFragment(fragmentMap.get(R.id.nav_home));
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -68,6 +74,33 @@ public class HomeActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+
+    private void checkIfUserIsAdmin() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) return;
+
+        String uid = currentUser.getUid();
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Account").child(uid);
+        userRef.child("role").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String role = snapshot.getValue(String.class);
+                    if ("ROLE_ADMIN".equals(role)) {
+                        Intent intent = new Intent(HomeActivity.this, AdminActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
     }
 
 
